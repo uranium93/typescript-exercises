6306ddc5099de4450b4f89a6a7c32ef73ff0e697
uranium93 <19hanafi93@gmail.com>
2020-05-14 21:05:44 +0400

    resolve exersice 10

--- exercises/exercise-10/declarations/str-utils/index.d.ts
+++ exercises/exercise-10/declarations/str-utils/index.d.ts
@@ -1,4 +1,7 @@
 declare module 'str-utils' {
-    // export const ...
-    // export function ...
+    export const strReverse: (param: string) => string;
+    export const strToUpper: (param: string) => string;
+    export const strRandomize: (param: string) => string;
+    export const strInvertCase: (param: string) => string;
+    export const strToLower: (param: string) => string;
 }
