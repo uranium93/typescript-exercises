ea0adb60c6f85b2dd7165ae48217d99962e70083
uranium93 <19hanafi93@gmail.com>
2020-05-19 15:27:01 +0400

    remove duplication

--- exercises/exercise-11/declarations/stats/index.d.ts
+++ exercises/exercise-11/declarations/stats/index.d.ts
@@ -1,9 +1,13 @@
 declare module 'stats' {
-    export function getMaxIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number;
-    export function getMinIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number;
-    export function getMedianIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number;
-    export function getMaxElement<T>(input: Array<T>, comparator: (a: T, b: T) => number): (T | null)
-    export function getMinElement<T>(input: Array<T>, comparator: (a: T, b: T) => number): (T | null)
-    export function getMedianElement<T>(input: Array<T>, comparator: (a: T, b: T) => number): (T | null)
-    export function getAverageValue<T>(input: Array<T>, comparator: (a: T, b: T) => number): (T | null)
+
+    type indexSearch = <T>(input: Array<T>, comparator: (a: T, b: T) => number) => number;
+    type elementSearch = <T>(input: Array<T>, comparator: (a: T, b: T) => number) => (T | null)
+
+    export const getMaxIndex: indexSearch
+    export const getMinIndex: indexSearch
+    export const getMedianIndex: indexSearch
+    export const getMaxElement: elementSearch
+    export const getMinElement: elementSearch
+    export const getMedianElement: elementSearch
+    export function getAverageValue<T>(input: Array<T>, comparator: (a: T, b: T) => number): (number | null)
 }
