a27e4791aa58a6fa880a1842b8d94a26aabf1bb0
uranium93 <19hanafi93@gmail.com>
2020-05-19 15:01:24 +0400

    resolve index case

--- exercises/exercise-11/declarations/stats/index.d.ts
+++ exercises/exercise-11/declarations/stats/index.d.ts
@@ -1,3 +1,4 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    export function getMaxIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number;
+    export function getMinIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number
 }
