2682b89900ba924ea983acb03ce8a2dbf290d1ea
uranium93 <19hanafi93@gmail.com>
2020-05-19 15:06:54 +0400

    resolve exercise 11

--- exercises/exercise-11/declarations/stats/index.d.ts
+++ exercises/exercise-11/declarations/stats/index.d.ts
@@ -1,4 +1,9 @@
 declare module 'stats' {
     export function getMaxIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number;
-    export function getMinIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number
+    export function getMinIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number;
+    export function getMedianIndex<T>(input: Array<T>, comparator: (a: T, b: T) => number): number;
+    export function getMaxElement<T>(input: Array<T>, comparator: (a: T, b: T) => number): (T | null)
+    export function getMinElement<T>(input: Array<T>, comparator: (a: T, b: T) => number): (T | null)
+    export function getMedianElement<T>(input: Array<T>, comparator: (a: T, b: T) => number): (T | null)
+    export function getAverageValue<T>(input: Array<T>, comparator: (a: T, b: T) => number): (T | null)
 }
