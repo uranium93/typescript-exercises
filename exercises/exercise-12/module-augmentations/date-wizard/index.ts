e5593e8109e7c4bc62dd96238d4e48f1de34459c
uranium93 <19hanafi93@gmail.com>
2020-05-20 14:07:02 +0400

    resolve exercise12

--- exercises/exercise-12/module-augmentations/date-wizard/index.ts
+++ exercises/exercise-12/module-augmentations/date-wizard/index.ts
@@ -2,5 +2,10 @@
 import 'date-wizard';
 
 declare module 'date-wizard' {
-    // Add your module extensions here.
+    export function pad(s: number): string
+    export interface DateDetails {
+        hours: number;
+        minutes: number;
+        seconds: number
+    }
 }
